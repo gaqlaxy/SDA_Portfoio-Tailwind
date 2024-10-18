2bc78dfe25bcf0f6ac11ded0d2172f60b0e0da0b
gaqlaxy <v4vripper@gmail.com>
2024-10-18 06:47:02 +0530

    Added Progess bar

---
+++ public/script.js
@@ -0,0 +1,91 @@
+// Progress bar 
+// Skill1
+var bar = new ProgressBar.Line(skill1, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: { width: '100%', height: '100%' }
+});
+
+bar.animate(0.95);  // Number from 0.0 to 1.0
+
+// Skill2
+var bar = new ProgressBar.Line(skill2, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: { width: '100%', height: '100%' }
+});
+
+bar.animate(0.90);  // Number from 0.0 to 1.0
+
+// Skill3
+var bar = new ProgressBar.Line(skill3, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: { width: '100%', height: '100%' }
+});
+
+bar.animate(0.80);  // Number from 0.0 to 1.0
+
+// Skill4
+var bar = new ProgressBar.Line(skill4, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: { width: '100%', height: '100%' }
+});
+
+bar.animate(0.90);  // Number from 0.0 to 1.0
+
+// Skill5
+var bar = new ProgressBar.Line(skill5, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: { width: '100%', height: '100%' }
+});
+
+bar.animate(0.75);  // Number from 0.0 to 1.0
+
+// Skill6
+var bar = new ProgressBar.Line(skill6, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: { width: '100%', height: '100%' }
+});
+
+bar.animate(0.95);  // Number from 0.0 to 1.0
+
+// Skill7
+var bar = new ProgressBar.Line(skill7, {
+    strokeWidth: 1.72,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: { width: '100%', height: '100%' }
+});
+
+bar.animate(0.80);  // Number from 0.0 to 1.0
